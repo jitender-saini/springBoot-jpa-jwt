136648f000b461c1e0ecfd96d9fa556361541504
Jitender Saini <iamjsaini@gmail.com>
2020-02-27 15:36:46 +0530

    Added AuthenticationController for custome login api

--- src/main/java/in/spring/config/security/WebSecurityConfig.java
+++ src/main/java/in/spring/config/security/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,6 +48,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
@@ -63,7 +70,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .authorizeRequests()
-                .antMatchers("/user/register").permitAll()
+                .antMatchers("/user/register", "/authenticate").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement()
---
+++ src/main/java/in/spring/controller/AuthenticationController.java
@@ -0,0 +1,85 @@
+package in.spring.controller;
+
+import in.spring.config.security.TokenService;
+import in.spring.config.security.model.JwtTokenRequest;
+import in.spring.config.security.model.JwtTokenResponse;
+import in.spring.config.security.userdetails.JwtUserDetailsService;
+import in.spring.repository.UserRepository;
+import in.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+public class AuthenticationController {
+
+    @Value("${jwt.http.request.header}")
+    private String tokenHeader;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    JwtUserDetailsService userDetailsService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping(value = "${jwt.get.token.uri}")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtTokenRequest authenticationRequest) throws Exception {
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        JwtTokenResponse jwtTokenResponse = tokenService.generateJwtToken(userDetails);
+        return ResponseEntity.ok(jwtTokenResponse);
+    }
+
+
+    @RequestMapping(value = "${jwt.refresh.token.uri}", method = RequestMethod.GET)
+    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) {
+        String authToken = request.getHeader(tokenHeader);
+        final String token = authToken.substring(7);
+        String username = tokenService.getUsernameFromToken(token);
+        if (tokenService.canTokenBeRefreshed(token)) {
+            String refreshedToken = tokenService.refreshToken(token);
+            return ResponseEntity.ok(JwtTokenResponse.bearer(token));
+        } else {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+
+    private void authenticate(String username, String password) throws Exception {
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
